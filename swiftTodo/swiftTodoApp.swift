a524dba3061fce370f013a9ba96c82cf3235bdaf
Mohammed Aabid A <mohammed@mavencart.com>
2022-05-05 14:28:34 +0530

    Initial Commit

---
+++ swiftTodo/swiftTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftTodoApp.swift
+//  swiftTodo
+//
+//  Created by Mohammed aabid on 05/05/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
